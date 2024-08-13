c17628276866e501e1600b6d8a0099b44e6a9fdb
malakalabttire <malakalbattire@Malaks-MacBook-Air.local>
2024-08-13 16:19:45 +0300

    orders page

--- lib/views/pages/order_pages /my_orders_page.dart
+++ lib/views/pages/order_pages /my_orders_page.dart
@@ -1,3 +1,72 @@
+// import 'package:e_commerce_app_flutter/views/widgets/order_tile_widget.dart';
+// import 'package:e_commerce_app_flutter/views/widgets/signin_signout_widget.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'package:e_commerce_app_flutter/provider/order_provider.dart';
+//
+// class MyOrdersPage extends StatelessWidget {
+//   const MyOrdersPage({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final orderProvider = Provider.of<OrderProvider>(context);
+//     final currentUser = FirebaseAuth.instance.currentUser;
+//
+//     // Store the last known user ID
+//     String? lastUserId;
+//
+//     WidgetsBinding.instance.addPostFrameCallback((_) {
+//       if (currentUser == null) {
+//         orderProvider.clearOrders();
+//       } else if (orderProvider.state == OrderState.initial ||
+//           currentUser.uid != lastUserId) {
+//         lastUserId = currentUser.uid;
+//         orderProvider.clearOrders(); // Clear previous user's orders
+//         orderProvider.loadOrders(currentUser.uid);
+//       }
+//     });
+//
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('My Orders'),
+//         centerTitle: true,
+//       ),
+//       body: RefreshIndicator(
+//         onRefresh: () async {
+//           await orderProvider.loadOrders(currentUser!.uid);
+//         },
+//         child: SingleChildScrollView(
+//           child: Padding(
+//             padding:
+//                 const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+//             child: Column(
+//               children: [
+//                 if (orderProvider.state == OrderState.loading)
+//                   const CircularProgressIndicator.adaptive()
+//                 else if (orderProvider.state == OrderState.error ||
+//                     currentUser == null)
+//                   SigninSignoutWidget()
+//                 else if (orderProvider.orders.isEmpty)
+//                   const Center(child: Text('No orders found'))
+//                 else ...[
+//                   for (var order in orderProvider.orders)
+//                     OrderTile(order: order),
+//                 ],
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// extension DateFormat on DateTime {
+//   String toShortDateString() {
+//     return '$day/$month/$year';
+//   }
+// }
 import 'package:e_commerce_app_flutter/views/widgets/order_tile_widget.dart';
 import 'package:e_commerce_app_flutter/views/widgets/signin_signout_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -5,8 +74,15 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:e_commerce_app_flutter/provider/order_provider.dart';
 
-class MyOrdersPage extends StatelessWidget {
+class MyOrdersPage extends StatefulWidget {
   const MyOrdersPage({super.key});
+
+  @override
+  _MyOrdersPageState createState() => _MyOrdersPageState();
+}
+
+class _MyOrdersPageState extends State<MyOrdersPage> {
+  String? lastUserId;
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +90,13 @@ class MyOrdersPage extends StatelessWidget {
     final currentUser = FirebaseAuth.instance.currentUser;
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (orderProvider.state == OrderState.initial) {
-        orderProvider.loadOrders(currentUser!.uid);
+      if (currentUser == null) {
+        orderProvider.clearOrders();
+      } else if (orderProvider.state == OrderState.initial ||
+          currentUser.uid != lastUserId) {
+        lastUserId = currentUser.uid;
+        orderProvider.clearOrders(); // Clear previous user's orders
+        orderProvider.loadOrders(currentUser.uid);
       }
     });
 
